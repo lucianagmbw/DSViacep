b73f2d0554488036011e345eb907ea21f34dc5b0
unknown <lucianagmbw@gmail.com>
2022-02-06 20:56:04 -0300

    constructor js

--- front/js/controllers/form-controller.js
+++ front/js/controllers/form-controller.js
@@ -1,5 +1,38 @@
+import Address from "../models/address.js";
+
+function State(){
+    
+    this.address = new Address();
+
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+const state = new State();
+
 export function init(){
 
-    const inputCep = document.forms.newAddress.cep;
-    console.log (inputCep);
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity = document.forms.newAddress.city;
+   
+
+    state.btnClear = document.forms.newAddress.btnClear;
+    state.btnSave = document.forms.newAddress.btnSave;
+
+    state.errorCep = document.querySelector('[data-error="cep"]');
+    state.errorNumber = document.querySelector('[data-error = "number"]');
+
+    console.log (state);
+
+
 }
